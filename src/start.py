9b1444837db6b5a3364ea15907d1cf1afb5486a8
amad3us47 <markus39107@gmail.com>
2026-04-03 12:34:55 +0530

    f

--- src/start.py
+++ src/start.py
@@ -2,7 +2,7 @@ from google import genai
 from mail.email import send_scrollhouse_welcome_email
 
 # Gemini setup
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key="AIzaSyBldSLbjEWtEhLLIvPon0GV-FyW5A1n4Ow")
 
 def generate_text(prompt: str) -> str:
     response = client.models.generate_content(
