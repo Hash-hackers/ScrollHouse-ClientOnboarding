875776c6831828bdc8003046a8674d2dd55a7335
amad3us47 <markus39107@gmail.com>
2026-04-03 12:33:51 +0530

    d

--- src/start.py
+++ src/start.py
@@ -1,7 +1,8 @@
 from google import genai
+from mail.email import send_scrollhouse_welcome_email
 
-# Create Gemini client
-client = genai.Client(api_key="AIzaSyBldSLbjEWtEhLLIvPon0GV-FyW5A1n4Ow")
+# Gemini setup
+client = genai.Client(api_key="GEMINI_API_KEY")
 
 def generate_text(prompt: str) -> str:
     response = client.models.generate_content(
@@ -9,3 +10,16 @@ def generate_text(prompt: str) -> str:
         contents=prompt
     )
     return response.text
+
+
+# Call the email sender
+send_scrollhouse_welcome_email(
+    generate_text=generate_text,
+    smtp_email="markus39107.com",
+    smtp_password="kkpx iadl iktk qbti",
+    client_name="Aisha",
+    brand_name="GlowLab",
+    client_email="aisha@glowlab.com",
+    account_manager="Rohan",
+    kickoff_calendar_link="https://calendly.com/kickoff"
+)
